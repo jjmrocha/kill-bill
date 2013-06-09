1d9e28bf09ffe3d6ae5ff718c357f6a362781ff0
jjmrocha <jjmrocha@localhost>
2013-06-09 01:40:47 +0100

    Implementar a execu??o de DTLs

--- src/kb_dtl_util.erl
+++ src/kb_dtl_util.erl
@@ -5,13 +5,41 @@
 %% ====================================================================
 -export([execute/3, execute/2]).
 
-execute(_Dtl, _Args) -> todo.
+execute(Dtl, Args) -> 
+	try render(Dtl, Args) of
+		Result -> Result
+	catch
+		_Type:_Error -> {error, not_found}
+	end. 
 
 execute(Dtl, none, Args) -> execute(Dtl, Args);
-execute(_Dtl, _Dict, _Args) -> todo.
+execute(Dtl, Dict, Args) ->
+	Fun = fun(Val) ->
+		case dict:find(Val, Dict) of
+			error -> "{" ++ Val ++ "}";
+			{ok, Text} -> Text
+		end
+	end,
+	Options = [{translation_fun, Fun}],
+	
+	try render(Dtl, Args, Options) of
+		Result -> Result
+	catch
+		_Type:_Error -> {error, not_found}
+	end. 
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
+render(Dtl, Args) ->
+	case Dtl:render(Args) of
+		{ok, IOList} -> {ok, IOList};
+		{error, Err} -> {error, Err}
+	end.
 
+render(Dtl, Args, Options) ->
+	case Dtl:render(Args, Options) of
+		{ok, IOList} -> {ok, IOList};
+		{error, Err} -> {error, Err}
+	end.
