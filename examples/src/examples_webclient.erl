b4c9f28b779a2b5902896cd9c6085b3956222d74
jjmrocha <jjmrocha@localhost>
2013-09-10 16:50:15 +0100

    Altera??o do exemplo do webclient

--- examples/src/examples_webclient.erl
+++ examples/src/examples_webclient.erl
@@ -15,24 +15,24 @@
 
 handle_init() -> 
 	error_logger:info_msg("~p: handle_init()\n", [?MODULE]),
-	{ok, dict:new()}.
+	{ok, []}.
 
 handle_client_connect(Client, State) -> 
 	error_logger:info_msg("~p: handle_client_connect(~p)\n", [?MODULE, Client]),
-	{ok, dict:store(Client, Client, State)}.
+	{ok, [Client|State]}.
 
 handle_client_cast(Client, Msg, State) ->
 	error_logger:info_msg("~p: handle_client_cast(~p, ~p)\n", [?MODULE, Client, Msg]),
-	send_msg(Msg, dict:fetch_keys(State)),
+	send_msg(Msg, remove(Client, State)),
 	{ok, State}.
 
 handle_client_disconnect(Client, State) ->
 	error_logger:info_msg("~p: handle_client_disconnect(~p)\n", [?MODULE, Client]),
-	{ok, dict:erase(Client, State)}.
+	{ok, remove(Client, State)}.
 
 handle_app_cast(Msg, State) ->
 	error_logger:info_msg("~p: handle_app_cast(~p)\n", [?MODULE, Msg]),
-	send_msg(Msg, dict:fetch_keys(State)),
+	send_msg(Msg, State),
 	{ok, State}.
 
 handle_app_call(Msg, State) ->
@@ -51,3 +51,6 @@ send_msg(_Msg, []) -> ok;
 send_msg(Msg, [Pid|T]) ->
 	Pid ! Msg,
 	send_msg(Msg, T).
+
+remove(Client, ClientList) ->
+	lists:delete(Client, ClientList).
