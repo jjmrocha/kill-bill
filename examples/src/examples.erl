500fa1b1869a2a64383a90aafeb58146f1350eda
jjmrocha <jjmrocha@localhost>
2013-11-28 12:18:40 +0000

    A nova vers?o do cowboy tem novas dependencias

--- examples/src/examples.erl
+++ examples/src/examples.erl
@@ -7,6 +7,7 @@ start() ->
 	ok = application:start(ranch),
 	ok = application:start(columbo),
 	ok = application:start(gibreel),
+	ok = application:start(cowlib),
 	ok = application:start(cowboy),
 	ok = application:start(kill_bill),
 	ok = application:start(examples).
